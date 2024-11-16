a85189d45ba0162876bb35ef241d8615cc9d80b4
Ankur2227 <2021ugcs060@nitjsr.ac.in>
2024-11-16 16:23:29 +0530

    added documentation for API

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import LandingPage from './Pages/LandingPage/LandingPage';
 import HomePage from './Pages/HomePage/HomePage';
 import CarDetailPage from './Pages/CarDetailPage/CarDetailPage';
 import EditCarPage from './Pages/EditCarPage/EditCarPage'; // Import the new EditCarPage component
+import Documentation from './Pages/Documentation/Documentation';
 
 function App() {
   return (
@@ -13,7 +14,8 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/homepage" element={<HomePage />} />
         <Route path="/car-detail/:id" element={<CarDetailPage />} />
-        <Route path="/edit-car/:id" element={<EditCarPage />} /> {/* Add the new route */}
+        <Route path="/edit-car/:id" element={<EditCarPage />} />
+        <Route path="/api/docs" element={<Documentation />} />
       </Routes>
     </div>
   );
---
+++ src/Pages/Documentation/Documentation.jsx
@@ -0,0 +1,10 @@
+const Documentation = () => {
+  return (
+    <ul>
+        <li><a href="https://documenter.getpostman.com/view/28385629/2sAYBPku6p">Docs for User APIs(Click here)</a></li>
+        <li><a href="https://documenter.getpostman.com/view/28385629/2sAYBPkuB8">Docs for User APIs(Click here):</a></li>
+    </ul>
+  )
+}
+
+export default Documentation
