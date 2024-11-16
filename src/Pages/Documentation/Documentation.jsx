f02506c3703b4f5b929fea039aecb6c2353f491b
Ankur2227 <2021ugcs060@nitjsr.ac.in>
2024-11-16 16:29:55 +0530

    minor fix

--- src/Pages/Documentation/Documentation.jsx
+++ src/Pages/Documentation/Documentation.jsx
@@ -2,7 +2,7 @@ const Documentation = () => {
   return (
     <ul>
         <li><a href="https://documenter.getpostman.com/view/28385629/2sAYBPku6p">Docs for User APIs(Click here)</a></li>
-        <li><a href="https://documenter.getpostman.com/view/28385629/2sAYBPkuB8">Docs for User APIs(Click here):</a></li>
+        <li><a href="https://documenter.getpostman.com/view/28385629/2sAYBPkuB8">Docs for Car APIs(Click here):</a></li>
     </ul>
   )
 }
